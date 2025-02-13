4f8f4ef5db7e1aac3c52ffa8115becc631d92df0
mostacho2000 <124900152+mostacho2000@users.noreply.github.com>
2025-02-13 13:17:04 -0600

    listo el codigo

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -7,33 +7,34 @@ using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
-    public Transform objetivo1;
-    public Transform objetivo2;
-    NavMeshAgent agente;
-    
-    bool CambiaObjetivo = false;
-    float tiempo = 0;
+    public Transform[] objetivos; // Array con los 7 objetivos
+    private NavMeshAgent agente;
+    private float tiempo = 0;
+    private float tiempoEspera = 14.0f; // Tiempo para cambiar de objetivo
+
     void Start()
     {
         agente = GetComponent<NavMeshAgent>();
-        agente.destination = objetivo1.position;
+        ElegirNuevoDestino();
     }
+
     void Update()
     {
-        if (tiempo>14.0f) {
+        tiempo += Time.deltaTime;
 
-            if (CambiaObjetivo)
-            {
-                agente.destination = objetivo2.position;
-                CambiaObjetivo = false;
-            }
-            else
-            {
-                agente.destination = objetivo1.position;
-                CambiaObjetivo = true;
-            }
+        if (tiempo > tiempoEspera)
+        {
+            ElegirNuevoDestino();
             tiempo = 0;
         }
-        tiempo += Time.deltaTime;
+    }
+
+    void ElegirNuevoDestino()
+    {
+        if (objetivos.Length == 0) return; // Evitar errores si no hay objetivos
+
+        int indiceAleatorio = Random.Range(0, objetivos.Length);
+        agente.destination = objetivos[indiceAleatorio].position;
     }
 }
+
